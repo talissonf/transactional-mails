03d9369cd576ba16ccbb991b77b630ce59c23b7b
Leonardo Matos <leomp120894@gmail.com>
2019-07-08 13:18:06 -0300

    feat(render): receive data object on render function

--- main.js
+++ main.js
@@ -10,38 +10,31 @@ const ejs = require('ejs')
 // https://github.com/sass/node-sass
 const sass = require('node-sass')
 
-module.exports = (template = 'index', lang = 'en_us', themeColor = '#6c757d') => {
+module.exports = (template = 'index', data = {}, lang = 'en_us', themeColor = '#6c757d') => {
   return new Promise((resolve, reject) => {
     // setup dictionary object first
     const i18n = path.join(process.cwd(), `i18n/${lang}.json`)
     fs.readFile(i18n, 'utf8', (err, contents) => {
-      let i18n
       if (err) {
         return reject(err)
       }
 
       try {
-        i18n = JSON.parse(contents)
+        data.i18n = JSON.parse(contents)
       } catch (err) {
         // invalid JSON
         return reject(err)
       }
 
       // render Sass to CSS with theme color
-      const renderSass = filename => sass.renderSync({
+      data.renderSass = filename => sass.renderSync({
         data: `$theme-color: ${themeColor};`,
         file: path.resolve(process.cwd(), `scss/${filename}.scss`)
       })
+      data.themeColor = themeColor
 
-      // setup data and parse template to HTML
+      // render EJS file to HTML
       const view = path.join(process.cwd(), `views/${template}.ejs`)
-      const data = {
-        i18n,
-        themeColor,
-        renderSass
-      }
-
-      // render EJS file
       ejs.renderFile(view, data, null, (err, html) => {
         if (err) {
           reject(err)
