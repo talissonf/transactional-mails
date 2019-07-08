09d44ec814d136808fb7eab431e01a4ca61b1c0d
Leonardo Matos <leomp120894@gmail.com>
2019-07-08 16:45:51 -0300

    docs(methods): starting templates docs on source

--- src/index.js
+++ src/index.js
@@ -9,10 +9,33 @@
 // template render function
 const render = require('./lib/render')
 
-const welcome = function () {
-  return render.apply(this, [ 'welcome', ...arguments ])
-}
+/**
+ * Default transactional email templates for E-Com Plus stores.
+ * @module @ecomplus/transactional-mails
+ * @exports {@link templates}
+ * @example
+ * const transactionalMails = require('@ecomplus/transactional-mails')
+ */
 
 module.exports = {
-  welcome
+
+  /**
+   * @method
+   * @memberof transactionalMails
+   * @description Returns HTML markup for customer welcome email.
+   * @param {object} customer - Customer body object
+   * @param {string} [lang='en_us'] - Snake case language code, eg.: 'en_us', 'pt_br'
+   * @param {string} [themeColor] - RGB color string
+   * @returns {Promise}
+   */
+
+  welcome (customer, lang, themeColor) {
+    const data = { customer }
+    return render('welcome', data, lang, themeColor)
+  }
+
 }
+
+/**
+ * @namespace transactionalMails
+ */
