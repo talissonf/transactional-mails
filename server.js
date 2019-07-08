ab702cfe12e7c0fc2a5fdd8af67074411c38aef7
Leonardo Matos <leomp120894@gmail.com>
2019-07-08 14:50:32 -0300

    test: setup dev server with browser sync

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const render = require('./main')
+// setup dev server with BrowserSync
+const browserSync = require('browser-sync').create()
+
+browserSync.init({
+  server: './test',
+  middleware: [
+    (req, res, next) => {
+      const template = req.url.replace('/', '')
+      if (template) {
+        render(template)
+          .then(html => res.end(html))
+          .catch(err => console.error(err))
+      } else {
+        next()
+      }
+    }
+  ],
+
+  // watch template source files and reload local server
+  watch: true,
+  files: [ 'views/*.ejs', 'scss/*.scss', 'i18n/*.json' ]
+})
