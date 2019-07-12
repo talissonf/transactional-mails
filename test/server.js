90249ba12ff810138b48f9fdb8d47d3cef63c50b
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2019-07-12 16:11:10 -0300

    chore(test): minor fixes in data and adding new templates in server

--- test/server.js
+++ test/server.js
@@ -2,7 +2,11 @@
 
 // load the lib object
 const { welcome } = require('./../src/')
+const { new_order } = require('./../src/')
 const { abandonned_cart } = require('./../src/')
+const { delivered } = require('./../src/')
+const { payment } = require('./../src/')
+const { shipped } = require('./../src/')
 // sample JSON data
 const store = require('./data/store.json')
 const customer = require('./data/customer.json')
@@ -25,7 +29,39 @@ browserSync.init({
     {
       route: '/abandonned_cart',
       handle (req, res, next) {
-        abandonned_cart(store, customer, cart)
+        abandonned_cart(store, customer, cart, 'pt_br')
+          .then(html => res.end(html))
+          .catch(err => console.error(err))
+      }
+    },
+    {
+      route: '/new_order',
+      handle (req, res, next) {
+        new_order(store, customer, order, 'pt_br')
+          .then(html => res.end(html))
+          .catch(err => console.error(err))
+      }
+    },
+    {
+      route: '/delivered',
+      handle (req, res, next) {
+        delivered(store, customer, order, 'pt_br')
+          .then(html => res.end(html))
+          .catch(err => console.error(err))
+      }
+    },
+    {
+      route: '/payment',
+      handle (req, res, next) {
+        payment(store, customer, order, 'pt_br')
+          .then(html => res.end(html))
+          .catch(err => console.error(err))
+      }
+    },
+    {
+      route: '/shipped',
+      handle (req, res, next) {
+        shipped(store, customer, order, 'pt_br')
           .then(html => res.end(html))
           .catch(err => console.error(err))
       }
